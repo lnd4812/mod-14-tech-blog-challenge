d80881fec2a4ca54b7d6222df0ead52809c08d70
LNDavid <ldavid@rmacan.com>
2022-06-02 12:20:34 -0400

    revise meta tag for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // connection to database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,  {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
