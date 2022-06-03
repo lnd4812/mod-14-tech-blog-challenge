8a9ad54fb9e336e9256b8e3381f2c38b878ac9d8
LNDavid <ldavid@rmacan.com>
2022-06-02 21:49:22 -0400

    add jawsdb credentials hail mary move

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // connection to database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
-    host: 'localhost',
+    host: 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
    
