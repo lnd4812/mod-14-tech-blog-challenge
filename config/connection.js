b93a6e87245a1e77f58bcd7ed6d6aad6c8a483c4
LNDavid <ldavid@rmacan.com>
2022-06-04 12:28:25 -0400

    edit

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 // connection to database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
     host: process.env.DB_HOST,
-    // host: 'localhost',
     dialect: 'mysql',
     port: 3306
    
