8bbd6bb747d25241043acf0266f1493cadacd648
LNDavid <ldavid@rmacan.com>
2022-06-04 12:36:52 -0400

    url

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // connection to database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
