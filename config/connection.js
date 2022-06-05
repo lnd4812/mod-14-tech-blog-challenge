06e8c5c865fb52fa858be23102da2c046fec6f7c
LNDavid <ldavid@rmacan.com>
2022-06-05 16:33:58 -0400

    reconfigure for heroku

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,9 @@ require('dotenv').config();
 
 // connection to database
 const sequelize = 
-// process.env.JAWSDB_URL 
-// ? new Sequelize(process.env.JAWSDB_URL)
-// : 
-new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
+process.env.JAWSDB_URL 
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
     host: process.env.DB_HOST,
     host: 'localhost',
     dialect: 'mysql',
